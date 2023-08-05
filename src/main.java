e32a27aec9124daf5d28f130ed93dd056d3c4925
Kozlova Natali <sipporte169523@gmail.com>
2023-08-05 10:27:52 +0300

    Добавлены изменения в main.java

--- src/main.java
+++ src/main.java
@@ -1,5 +1,14 @@
+import java.util.List;
+
 public class main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        ToyStore toyStore = new ToyStore();
+
+        toyStore.put("1", "Constructor", 2);
+        toyStore.put("2", "Robot", 2);
+        toyStore.put("3", "Doll", 6);
+
+        List <Toy> toys = toyStore.getToys(10);
+        toyStore.writeToFile(toys, "results.txt");
     }
 }
