46da9e0390955f08ad70daa7a3cf32f2ca20dd36
Kozlova Natali <sipporte169523@gmail.com>
2023-08-05 10:27:22 +0300

    Добавлены изменения в Toy.java

--- src/Toy.java
+++ src/Toy.java
@@ -1,11 +1,4 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Random;
-
-class Toy implements Comparable&lt;Toy&gt; {
+class Toy implements Comparable <Toy> {
     private String toyId;
     private String name;
     private int weight;
